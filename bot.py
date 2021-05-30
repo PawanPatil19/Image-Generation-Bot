53d9dad460971922caf10081afe1b555d0171503
Pawan Patil <63651567+PawanPatil19@users.noreply.github.com>
2021-05-30 14:49:10 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
-#Bot to upload contest posts to Instagram
 import discord
 from discord.ext import commands
 import datetime
 import asyncio
 import random
+import requests
 from instabot import Bot
-
+from instabot import utils
+import os
+import shutil
 from PIL import Image, ImageFont, ImageDraw
 from io import BytesIO
+
 
 
 bot = Bot()
@@ -16,6 +19,14 @@ bot.login(username="******", password="*******")
 
 client = commands.Bot(command_prefix = ">")
 client.remove_command("help")
+
+def clean_up():
+	dir = "config"
+	if os.path.exists(dir):
+		try:
+			shutil.rmtree(dir)
+		except OSError as e:
+			print("Error : %s - %s." % (e.filename, e.strerror))	
 
 @client.event
 async def on_ready():
@@ -28,8 +39,9 @@ async def help(ctx):
 	em.add_field(name = "name", value="Contest Name")
 	em.add_field(name = "time", value="Contest Time")
 	em.add_field(name = "date", value="Contest Date")
-	em.add_field(name = "upload_mirror", value="Upload to Instagram")
+	em.add_field(name = "upload_mirror", value="Upload Mirror to Instagram")
 	em.add_field(name = "upload_beginner", value="Upload to Instagram")
+	em.add_field(name = "upload_ln", value="Upload to Linkedin")
 
 	await ctx.send(embed = em)
 
@@ -80,6 +92,11 @@ async def time(ctx, *, text = "No test entered"):
 	
 	await ctx.send(embed = embed)
 	await ctx.send(file = discord.File("c2.jpg"))
+	channel = client.get_channel(754069393142710422)
+	id = channel.get_guild(781093099601788954)
+	channel1 = client.get_channel(789411627190583326)
+	await channel.send(file = discord.File("c2.jpg"))
+	await channel1.send(file = discord.File("c2.jpg"))
 
 
 
@@ -101,7 +118,10 @@ async def upload_beginner(ctx, *, text):
 	embed=discord.Embed(title="Uploaded", description=upload_text, color=0x00ff00)
 	
 	await ctx.send(embed = embed)
-	bot.upload_photo("c2.jpg", caption= upload_text)
+	bot.upload_photo("c2.jpg", caption= upload_text)	
+	embed1=discord.Embed(title="Upload to Linkedin", description="https://www.linkedin.com/company/72295847/admin/", color=0x00ff00)
+	
+	await ctx.send(embed = embed1)
 	
 @client.command()
 async def upload_mirror(ctx,*, text):	
@@ -121,18 +141,23 @@ async def upload_mirror(ctx,*, text):
 	embed=discord.Embed(title="Uploaded", description=upload_text, color=0x00ff00)
 	
 	await ctx.send(embed = embed)
-	bot.upload_photo("c2.jpg", caption= upload_text)	
-
-@client.command()
-async def ping(ctx):
-	channel = client.get_channel(*************)
-	await channel.send(file = discord.File("c2.jpg"))
+	bot.upload_photo("c2.jpg", caption= upload_text)
+	embed1=discord.Embed(title="Upload to Linkedin", description="https://www.linkedin.com/company/72295847/admin/", color=0x00ff00)	
+	await ctx.send(embed = embed1)	
 	
 
+@client.group(invoke_without_command=True)
+async def upload_ln(ctx):
+	embed1=discord.Embed(title="Upload to Linkedin", description="https://www.linkedin.com/company/72295847/admin/", color=0x00ff00)	
+	await ctx.send(embed = embed1)
 
 
 
-client.run("******)
+
+
+client.run("******")
 
 
 
+if __name__ == '__main__':
+	clean_up()
